431c05225e9f91c0716a8a565798d5c6a5b4aa02
Vasily Shumikhin <vasily.shumikhin@grapecity.com>
2021-04-07 16:06:17 +0700

    Added population class

---
+++ ProjectNeuroevolution/NeuralNetwork/Core/Implementations/Population/Population.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Core.Implementations.Population
+{
+    internal class Population
+    {
+        List<object> _individuals;
+        List<object> _breedingPool;
+
+        void TournamentSelect()
+        {
+
+        }
+
+        void RankSelect() 
+        {
+            
+        }
+    }
+}
